582ec4633f78eeb29f8804b264b8cb437842a641
vincent <820203435@qq.com>
2018-04-23 14:09:51 +0800

    change port

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.db_url = 'mongodb://emrs:emrs123@116.62.148.24:27017/emrs';//mongodb://116.62.148.24:27017/emrs';
-exports.server_url = 'http://localhost:3000/';
-// exports.server_url = 'http://aidr.vip:3000/';
+// exports.server_url = 'http://localhost:3000/';
+exports.server_url = 'http://aidr.vip:8008/';
 exports.origin_path = __dirname;
